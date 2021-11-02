c592cfe46124cf8355998ae0c669677535de0c27
“Jray810” <jia.raymond19@gmail.com>
2021-11-02 15:58:09 -0400

    Uploaded provided template

---
+++ cs4400_phase2_database_template_v0.sql
@@ -0,0 +1,18 @@
+-- CS4400: Introduction to Database Systems (Fall 2021)
+-- Phase II: Create Table & Insert Statements [v0] Thursday, October 14, 2021 @ 2:00pm EDT
+
+-- Team __
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+-- Team Member Name (GT username)
+
+-- Directions:
+-- Please follow all instructions for Phase II as listed on Canvas.
+-- Fill in the team number and names and GT usernames for all members above.
+-- Create Table statements must be manually written, not taken from an SQL Dump file.
+-- This file must run without error for credit.
+
+-- ------------------------------------------------------
+-- CREATE TABLE STATEMENTS AND INSERT STATEMENTS BELOW
+-- ------------------------------------------------------
