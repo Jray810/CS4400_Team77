c174d04fbb43277084fd76b79f9579fcd5521cbf
“Jray810” <jia.raymond19@gmail.com>
2021-12-03 17:31:27 -0500

    Added testing for stored procedure 1b

--- Phase 3/cs4400_phase3_testing.sql
+++ Phase 3/cs4400_phase3_testing.sql
@@ -10,8 +10,9 @@ USE travel_reservation_service;
 -- Testing
 -- --------------------------------------------------------------------------
 
+-- --------------------------------------------------------------------------
 -- [1a] Test Procedure: register_customer
-
+-- --------------------------------------------------------------------------
 -- Add new customer (Does not exist in DB, Email Not-Unique Only): Expect 0 row(s) affected
 CALL register_customer('aray@tiktok.com', 'Michael', 'Jackson', 'mjackson2021', '111-111-1111', '1111 1111 1111 1111', '111', '2019-01-01', 'USA');
 -- Add new customer (Does not exist in DB, Phone Number Not-Unique Only): Expect 0 row(s) affected
@@ -47,5 +48,34 @@ CALL register_customer('aray@tiktok.com', 'Peter', 'Parker', 'spidermanRules', '
 -- Add new customer (Exists as Account, Client, and Customer, no field changes): Expect 0 row(s) affected
 CALL register_customer('aray@tiktok.com', 'Addison', 'Ray', 'password17', '770-234-5678', '3110 2669 7949 5605', '744', '2022-08-01', '');
 
-
+-- --------------------------------------------------------------------------
 -- [1b] Test Procedure: register_owner
+-- --------------------------------------------------------------------------
+-- Add new owner (Does not exist in DB, Email Not-Unique Only): Expect 0 row(s) affected
+CALL register_owner('aray@tiktok.com', 'Michael', 'Jackson', 'mjackson2021', '111-111-1111');
+-- Add new owner (Does not exist in DB, Phone Number Not-Unique Only): Expect 0 row(s) affected
+CALL register_owner('mjackson@gmail.com', 'Michael', 'Jackson', 'mjackson2021', '404-678-5555');
+-- Add new owner (Does not exist in DB): Expect new entry in accounts, clients, and owners
+CALL register_owner('mjackson@gmail.com', 'Michael', 'Jackson', 'mjackson2021', '111-111-1111');
+SELECT * FROM accounts AS A LEFT OUTER JOIN clients AS C ON A.Email = C.Email LEFT OUTER JOIN owners AS O ON C.Email = O.Email WHERE A.Email = 'mjackson@gmail.com';
+
+-- Add new owner (Exists as Account only, Account Fields Do Not Match, Key Fields Unique): Expect 0 row(s) affected
+CALL register_owner('mmoss1@travelagency.com', 'Bob', 'Ross', 'password12', '222-222-2222');
+-- Add new owner (Exists as Account only, Account Fields Match, Phone Number Not-Unique Only): Expect 0 row(s) affected
+CALL register_owner('mmoss1@travelagency.com', 'Mark', 'Moss', 'password1', '404-678-5555');
+-- Add new owner (Exists as Account only, Account Fields Match, Key Fields Unique): Expect new entry in clients and owners
+CALL register_owner('mmoss1@travelagency.com', 'Mark', 'Moss', 'password1', '222-222-2222');
+SELECT * FROM accounts AS A LEFT OUTER JOIN clients AS C ON A.Email = C.Email LEFT OUTER JOIN owners AS O ON C.Email = O.Email WHERE A.Email = 'mmoss1@travelagency.com';
+
+-- Add new owner (Exists as Account and Client, Account Fields Do Not Match): Expect 0 row(s) affected
+CALL register_owner('aray@tiktok.com', 'Peter', 'Parker', 'spidermanRules', '770-234-5678');
+-- Add new owner (Exists as Account and Client, Client Fields Do Not Match): Expect 0 row(s) affected
+CALL register_owner('aray@tiktok.com', 'Addison', 'Ray', 'password17', '333-333-3333');
+-- Add new owner (Exists as Account and Client, Account/Client Fields Match): Expect new entry in owners
+CALL register_owner('aray@tiktok.com', 'Addison', 'Ray', 'password17', '770-234-5678');
+SELECT * FROM accounts AS A LEFT OUTER JOIN clients AS C ON A.Email = C.Email LEFT OUTER JOIN owners AS O ON C.Email = O.Email WHERE A.Email = 'aray@tiktok.com';
+
+-- Add new owner (Exists as Account, Client, and Owner, field changes): Expect 0 row(s) affected
+CALL register_owner('arthurread@gmail.com', 'Peter', 'Parker', 'spidermanRules', '555-234-5678');
+-- Add new owner (Exists as Account, Client, and Owner, no field changes): Expect 0 row(s) affected
+CALL register_owner('arthurread@gmail.com', 'Arthur', 'Read', 'password4', '555-234-5678');
